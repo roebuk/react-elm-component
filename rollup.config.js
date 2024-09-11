7852c0a09be8bc56b8817f3c47b1ba9ce66dc6bd
Kristian Roebuck <kristian.roebuck@mettle.co.uk>
2024-09-11 12:05:40 +0100

    Update deps and switch to ESM online

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,14 @@
-import { terser } from 'rollup-plugin-terser';
+import terser from "@rollup/plugin-terser";
 
 export default {
-  external: ['react'],
-  input: 'index.js',
-  output: [{
-    file: 'dist/rec.js',
-    format: 'cjs',
-    name: 'version',
-    plugins: [terser()]
-  }, {
-    file: 'dist/rec.esm.js',
-    format: 'esm',
-    name: 'version',
-    plugins: [terser()]
-  }]
+  external: ["react"],
+  input: "index.js",
+  output: [
+    {
+      file: "dist/react-elm-component.mjs",
+      format: "esm",
+      name: "version",
+      plugins: [terser()],
+    },
+  ],
 };
